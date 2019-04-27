165544b147e9c3aa98e49806a9615aaac0c8b385
Chandni Soni <soni.chandni.415@gmail.com>
2019-04-27 21:56:27 +0530

    Updated Portfolio

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,13 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+$('.nav-link').on('click', function() {  
+    menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //set menu state
+        showMenu = false;
+});
